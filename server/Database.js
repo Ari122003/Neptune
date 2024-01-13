06754e35b2f58a0648e6eb8dcf937c7528164829
Aritra <aritraadhikary2003@gmail.com>
2024-01-13 21:39:11 +0530

    Deploy

--- server/Database.js
+++ server/Database.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
+import { MongoClient } from "mongodb";
 mongoose.set("strictQuery", true);
 
 // const url = "mongodb://127.0.0.1:27017/Neptune";
 const url =
 	"mongodb+srv://Aritra:MbchAudwK4d6lAoA@cluster0.tog5hdg.mongodb.net/";
+
+const client = new MongoClient(url);
 
 const connect = async () => {
 	await mongoose.connect(url).then(() => {
